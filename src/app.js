34ce5df0af3b19df59039d8a2fafde0ea08c789f
Andrei <a.g.popov@hotmail.com>
2022-12-04 23:31:18 +0100

    fixed eslint remark

--- src/app.js
+++ src/app.js
@@ -5,11 +5,10 @@ import { getQuizData } from './utils/sessionStorage.js';
 const savedQuizData = getQuizData();
 
 const loadApp = () => {
-  // eslint-disable-next-line no-extra-boolean-cast
-  if (!!savedQuizData) {
-    initQuestionPage();
-  } else {
+  if (savedQuizData === null) {
     initWelcomePage();
+  } else {
+    initQuestionPage();
   }
 };
 
